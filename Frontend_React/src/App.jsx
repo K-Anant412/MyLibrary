a2ebc2013998fd829bef50837a9627e6c481c91c
K-Anant412 <anantkore412@gmail.com>
2026-05-31 23:17:25 +0530

    srating an login page for Library

--- Frontend_React/src/App.jsx
+++ Frontend_React/src/App.jsx
@@ -1,10 +1,51 @@
 import { useState } from 'react'
+import { House, BookSearch, BookHeart, Heart, LogOut } from 'lucide-react'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 className='bg-amber-600'>Hello, tailwind CSS,</h1>
+      <nav className='w-[250px] h-screen bg-amber-100 flex flex-col p-3'>
+        
+        <div className='w-full p-1.5 h-[30%] border'>  
+          <h1 className='text-[35px] font-extrabold'>InkWell</h1>
+          <p>Never run out of pages</p>
+        </div>
+
+        <div className='h-[55%] border flex items-center justify-center'>
+            <ul className='h-[80%] w-[80%] flex flex-col justify-around items-center'>
+
+              <li className=' w-full flex'>
+                <House />
+                Home
+              </li>
+
+              <li className=' w-full flex'>
+                <BookSearch />
+                Explore
+              </li>
+
+              <li className=' w-full flex'>
+                <BookHeart />
+                My Library
+              </li>
+
+              <li className=' w-full flex'>
+                <Heart />
+                Favorite
+              </li>
+
+            </ul>
+        </div>
+      
+      <div className='h-[15%] border'>
+        <button>
+          <LogOut />
+          Log Out
+        </button>
+      </div>
+
+      </nav>
     </>
   )
 }
