6d10678c610b40b36539203ca25dfb62ef9e8678
K-Anant412 <anantkore412@gmail.com>
2026-06-18 19:25:52 +0530

    pause the project work to understand some UI libraries of React

--- Frontend_React/src/components/Navbar.jsx
+++ Frontend_React/src/components/Navbar.jsx
@@ -7,82 +7,78 @@ import { AiFillProduct } from 'react-icons/ai'
 
 const Navbar = () => {
 
- 
+    const isActiveLink = ({ isActive })=>
+                                ` w-[70%] flex items-center justify-center border border-[#E7E1B1] rounded-2xl px-4 py-2 cursor-pointer gap-1 
+                                ${
+                                    isActive
+                                        ? "bg-[#E7E1B1] scale-105"
+                                        : "border-[#19701bf5] hover:bg-[#19701bf5]"
+                                }`
 
-  return (
-    <>
-      <nav className='w-70 h-full flex flex-col bg-[#0a420b] shrink-0 shadow-black shadow-lg z-50 relative top-2 rounded-tr-3xl'>
+    return (
+        <>
+        <nav
+            className="
+                w-72 h-[95%]
+                flex flex-col
+                bg-[#bbf7d0]  {/* Assuming this is matching your exact green background */}
+                shrink-0 z-50
+                relative top-4 left-5
+                border
+                border-[#a2d1b3]
+                rounded-[20px]
+                shadow-[13px_14px_30px_rgba(8,85,9,0.2),-12px_-12px_40px_rgba(2,122,27,0.3)]
+                transition-all duration-300
+            "
+            >
 
-            <div className=' w-full h-[30%] flex flex-col pt-6 gap-1.5 items-center relative top-6.5'>
-                <h1 className='flex items-center gap-1.5 text-4xl font-bold w-full text-[#ccc592] hover:underline font-[DM_Serif_Text] relative left-12'>
-                    < BiSolidLeaf size={40} />
-                    InkWell
-                </h1>
-                <h3 className='text-2xl font-semibold w-full text-[#ccc592] h-full text-center relative -left-0.5'>Start reading.</h3>
-            </div>
+                <div className=' w-full h-[30%] flex flex-col pt-6 gap-1.5 items-center relative top-6.5'>
+                    <h1 className='flex items-center gap-1.5 text-4xl font-bold w-full text-[#76714a] hover:underline font-[DM_Serif_Text] relative left-12'>
+                        < BiSolidLeaf size={40} />
+                        InkWell
+                    </h1>
+                    <h3 className='text-2xl font-semibold w-full text-[#706d50] h-full text-center relative -left-0.5'>Start reading.</h3>
+                </div>
 
-            <div className='w-full h-[60%]'>
-                <ul className='list-none w-full h-full flex flex-col items-center gap-4'>
+                <div className='w-full h-[60%]'>
+                    <ul className='list-none w-full h-full flex flex-col items-center gap-4'>
 
-                    <NavLink 
-                        to="/" 
-                        className={({ isActive })=>
-                            ` w-[70%] flex items-center justify-center border-2 border-[#E7E1B1] rounded-2xl px-4 py-2 cursor-pointer gap-1 rounded-br-[40px] rounded-tr-[40px] 
-                            ${
-                                isActive
-                                    ? "bg-[#E7E1B1] text-[#0D530E] scale-105"
-                                    : "border-[#E7E1B1] text-[#0a450b] hover:bg-[#0a450b]"
-                            }`
-                        }>
-                        <span className='text-[24px] text-[#ccc592] font-bold relative -top-0.5'>Home</span>
-                    </NavLink>
+                        <NavLink 
+                            to="/" 
+                            className={isActiveLink}>
+                            <span className='text-[24px] font-bold relative -top-0.5'>Home</span>
+                        </NavLink>
 
-                    <NavLink to="/explore" className='w-[70%] flex items-center justify-center border-2 border-[#E7E1B1] rounded-2xl px-4 py-2 cursor-pointer gap-1 rounded-br-[40px] rounded-tr-[40px]' >
-                        <span className='text-[24px] text-[#ccc592] font-bold relative -top-0.5'>Browse Books</span>
-                    </NavLink>
+                        <NavLink 
+                            to="/explore" 
+                            className={isActiveLink}>
+                            <span className='text-[24px]  font-bold relative -top-0.5'>Browse Books</span>
+                        </NavLink>
 
-                    <NavLink to="/mylibrary" className='w-[70%] flex items-center justify-center border-2 border-[#E7E1B1] rounded-2xl px-4 py-2 cursor-pointer gap-1 rounded-br-[40px] rounded-tr-[40px]' >
-                        <span className='text-[24px] text-[#ccc592] font-bold relative -top-0.5'>My Library</span>
-                    </NavLink>
+                        <NavLink 
+                            to="/mylibrary" 
+                            className={isActiveLink}>
+                            <span className='text-[24px]  font-bold relative -top-0.5'>My Library</span>
+                        </NavLink>
 
-                    <NavLink to="/membership" className='w-[70%] flex items-center justify-center border-2 border-[#E7E1B1] rounded-2xl px-4 py-2 cursor-pointer gap-1 rounded-br-[40px] rounded-tr-[40px]' >
-                        <span className='text-[24px] text-[#ccc592] font-bold relative -top-0.5'>Memberships</span>
-                    </NavLink>
+                        <NavLink 
+                            to="/membership" 
+                            className={isActiveLink}>
+                            <span className='text-[24px]  font-bold relative -top-0.5'>Memberships</span>
+                        </NavLink>
 
-                    
-                    
-                    {/* <NavLink to="/" className={linkStyles}>
-                        <FiHome size={23} />
-                        <span>Home</span>
-                    </NavLink>
+                    </ul>
+                </div>
 
-                    <NavLink to="/search" className={linkStyles}>
-                        <AiFillProduct size={23} />
-                        <span>Product</span>
-                    </NavLink>
+                <div className='w-full h-[10%] flex justify-center'>
+                    <button className='w-[70%] h-12 border-[#E7E1B1] border rounded-2xl text-[26px] text-[#ccc592] font-extrabold flex items-center justify-center  relative -top-5'>
+                        Log Out
+                    </button>
+                </div>
 
-                    <NavLink to="/about" className={linkStyles}>
-                        <MdAccountCircle size={23} />
-                        <span>About</span>
-                    </NavLink>
+            </nav>
+        </>
+    )
+    }
 
-                    <NavLink to="/contact" className={linkStyles}>
-                        <MdAddCall size={23} /> 
-                        <span>Contact</span>
-                    </NavLink> */}
-
-                </ul>
-            </div>
-
-            <div className='w-full h-[10%] flex justify-center'>
-                <button className='w-[70%] h-12 border-[#E7E1B1] border rounded-2xl text-[26px] text-[#ccc592] font-extrabold flex items-center justify-center  relative -top-5'>
-                    Log Out
-                </button>
-            </div>
-
-        </nav>
-    </>
-  )
-}
-
-export default Navbar
+    export default Navbar
--- Frontend_React/src/components/pages/Home.jsx
+++ Frontend_React/src/components/pages/Home.jsx
@@ -7,9 +7,9 @@ const  Home= () => {
       <section className='w-full h-full bg-green-200 flex items-center justify-center flex-col overflow-y-scroll scrollbar-none'>
           <h1 className='text-4xl font-extrabold'>Start your reading journy.</h1>
 
-          <div id='head_part' className='w-full p-1 h-112.5 flex items-center justify-center border'>
+          {/* <div id='head_part' className='w-full p-1 h-112.5 flex items-center justify-center border'>
 
-          </div>
+          </div> */}
           
       </section>
     </>
