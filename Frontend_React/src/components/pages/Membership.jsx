a65276ddb5129c950d63afde887ad9725d593b83
K-Anant412 <anantkore412@gmail.com>
2026-06-24 23:04:42 +0530

    membership UI

--- Frontend_React/src/components/pages/Membership.jsx
+++ Frontend_React/src/components/pages/Membership.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 const Membership = () => {
   return (
     <>
-      <section className='w-full h-full border rounded-2xl bg-green-200 flex items-center justify-center'>
-          <h1 className='text-6xl font-extrabold'>Membership</h1>
+      <section className='w-full h-full flex items-center justify-center gap-7'>
+          
+          <div className='w-[30%] h-[70%] border-2 border-[#E7E1B1] rounded-2xl bg-[#e7e1b1dc] shadow-2xl shadow-black flex flex-col items-center p-10'>
+
+            <button className='w-[60%] h-10 text-[30px] font-bold flex items-center justify-center border-2 border-[#1e7a29e3] rounded-2xl bg-[#1e7a29f2]'>Purchase</button>
+          </div>
+          <div className='w-[30%] h-[70%] border-2 border-[#E7E1B1] rounded-2xl bg-[#e7e1b1dc] shadow-2xl shadow-black '>
+
+          </div>
+          <div className='w-[30%] h-[70%] border-2 border-[#E7E1B1] rounded-2xl bg-[#e7e1b1dc] shadow-2xl shadow-black '>
+
+          </div>
+
       </section>
     </>
   )
