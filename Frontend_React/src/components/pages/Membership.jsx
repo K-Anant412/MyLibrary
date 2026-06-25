7c635fb4d037d7f1a5b3bb5e84104840dc5b6fd7
K-Anant412 <anantkore412@gmail.com>
2026-06-25 20:38:06 +0530

    starting new UI design, and renaming the old folders

--- Frontend_React/src/components/pages/Membership.jsx
+++ Frontend_React/src/components/pages/Membership.jsx
@@ -3,18 +3,15 @@ import React from 'react'
 const Membership = () => {
   return (
     <>
-      <section className='w-full h-full flex items-center justify-center gap-7'>
+      <section className='w-full h-full flex items-center gap-7'>
           
           <div className='w-[30%] h-[70%] border-2 border-[#E7E1B1] rounded-2xl bg-[#e7e1b1dc] shadow-2xl shadow-black flex flex-col items-center p-10'>
+            <div className='w-full h-[60%] border rounded-2xl'>
 
-            <button className='w-[60%] h-10 text-[30px] font-bold flex items-center justify-center border-2 border-[#1e7a29e3] rounded-2xl bg-[#1e7a29f2]'>Purchase</button>
+            </div>
+            <button className='text-white w-[60%] h-10 text-[30px] font-bold flex items-center justify-center border-2 border-[#1e7a29e3] rounded-2xl bg-[#1e7a29f2]'>Purchase</button>
           </div>
-          <div className='w-[30%] h-[70%] border-2 border-[#E7E1B1] rounded-2xl bg-[#e7e1b1dc] shadow-2xl shadow-black '>
-
-          </div>
-          <div className='w-[30%] h-[70%] border-2 border-[#E7E1B1] rounded-2xl bg-[#e7e1b1dc] shadow-2xl shadow-black '>
-
-          </div>
+          
 
       </section>
     </>
