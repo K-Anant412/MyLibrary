01a0fb627b21a2aae99a782b0b33088c6eb2a040
K-Anant412 <anantkore412@gmail.com>
2026-06-28 19:10:30 +0530

    login page complete

--- App_UI/src/pages/LoginCard.jsx
+++ App_UI/src/pages/LoginCard.jsx
@@ -1,48 +1,18 @@
-import { useState } from 'react'
-import background_image_one from './images/background_image_one.jpg'
+import React from 'react'
 
-import { useForm } from 'react-hook-form' 
 
-import { FaRegUserCircle } from 'react-icons/fa'
-import { SiMaildotru } from "react-icons/si";
-import { GiBookmark } from "react-icons/gi";
-import { BsThreeDots } from "react-icons/bs";
-import { BsGoogle } from "react-icons/bs";
-import { RiWhatsappFill } from "react-icons/ri";
-import { AiFillInstagram } from "react-icons/ai";
-import { SiApacheairflow } from "react-icons/si";
 
-import './App.css'
+const LoginCard = () => {
 
-function App() {
-
-  const [user, setUser] = useState("")
-  const [email, setEmail] = useState("")
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
-
-  const onSubmit = (data) => console.log(data)
 
   return (
     <>
-      <section 
-        className='w-screen h-screen flex items-center bg-cover bg-center' 
-        style={{
-          backgroundImage: `url(${background_image_one})`
-        }}
-        >
-      
-        <div className='w-[48%] h-[85%] absolute right-0 bg-white/30 backdrop-blur-md border border-white/20 rounded-tl-2xl rounded-bl-2xl p-8 shadow-lg flex items-center justify-center overflow-hidden'>
+      <div className='w-[48%] h-[85%] absolute right-0 bg-white/30 backdrop-blur-md border border-white/20 rounded-tl-2xl rounded-bl-2xl p-8 shadow-lg flex items-center justify-center overflow-hidden'>
           
           <div className='pt-3 w-[55%] h-[85%] border-2 rounded-3xl flex flex-col items-center justify-center border-none shadow-inner shadow-[#5a5959de] bg-[#f9e3c4] relative overflow-hidden'>
             
             <div className='absolute w-2 h-225 bg-[#9c2754ee] right-4 z-0'></div>
-            <div className='absolute w-6 h-225 bg-[#9c2754] right-8 z-1 rotate-90 top-7'></div>
+            <div className='absolute w-6 h-225 bg-[#9c2754] right-8 z-1 rotate-90 top-10'></div>
 
             <div className='w-full h-20  flex items-center justify-center relative -top-22 z-20'>
 
@@ -56,7 +26,6 @@ function App() {
             </div>
              
              <form 
-              onSubmit={handleSubmit(onSubmit)}
               className='w-full h-fit p-1 flex flex-col items-center justify-center gap-4 relative -top-15 z-20' 
              >
 
@@ -69,7 +38,7 @@ function App() {
                 <input 
                   placeholder='Username' 
                   {...register("username")} 
-                  className='placeholder:text-[#656363] font-sans px-12 w-[80%] h-12 border rounded-2xl text-2xl font-medium rounded-bl-3xl z-20 rounded-tl-3xl border-[#6b3449d2]'  
+                  className='placeholder:text-[#656363] px-12 w-[80%] h-12 border rounded-2xl text-2xl font-bold rounded-bl-3xl z-20 rounded-tl-3xl border-[#6b3449d2]'  
                 />
 
               </div>
@@ -83,13 +52,14 @@ function App() {
               <input 
                 placeholder='Email'
                 {...register("email")} 
-                className='placeholder:text-[#656363] font-sans px-12 w-[80%] h-12 border rounded-2xl text-2xl font-medium rounded-bl-3xl rounded-tl-3xl text-[#656363] border-[#6b3449d2]'    
+                className='placeholder:text-[#656363] px-12 w-[80%] h-12 border rounded-2xl text-2xl font-bold rounded-bl-3xl rounded-tl-3xl text-[#656363] border-[#6b3449d2]'    
                 />
               
               </div>
 
               <button
                 className='w-[70%] h-12 border rounded-4xl text-2xl font-bold font-sans bg-[#9c2754ee] transition duration-300 text-white hover:-translate-y-0.5 bg-[#9c2754fe] cursor-pointer '
+                onClick={handleSubmit(onSubmit)}
               >
                 Log In
               </button>
@@ -103,7 +73,7 @@ function App() {
               
             </form>
 
-            <div className='w-full h-10 py-2.5 flex items-center justify-center font-semibold font-sans absolute bottom-30 gap-1'>
+            <div className='w-full h-10 py-2.5 flex items-center justify-center font-semibold font-sans absolute bottom-36 gap-1'>
               <span>
                 <BsThreeDots size={25} />
               </span>
@@ -113,7 +83,7 @@ function App() {
               </span>
             </div>
 
-            <div className='w-full h-35 absolute bottom-7 px-11 flex items-center justify-center gap-6'>
+            <div className='w-full h-35 absolute bottom-12 px-11 flex items-center justify-center gap-6'>
                 <a href="#">
                     <AiFillInstagram size={35} className='text-[#656363] cursor-pointer w-10 h-10 border p-1 rounded-[50%] shadow-black inset-1 transition-all duration-200 hover:rounded-xl' />
                 </a> 
@@ -128,22 +98,8 @@ function App() {
             </div>
           </div>
         </div>
-
-        <div className='w-[50%] h-[50%] flex flex-col items-center justify-center text-white'>
-          <h1 
-            className='w-full h-40 flex items-center text-[100px] font-bold relative left-10 top-12 text-shadow-lg text-white transition-all duration-75 hover:text-transparent [-webkit-text-stroke:2px_white]'
-          >Read.</h1>
-          <h1 
-            className='w-full h-40 flex items-center text-[100px] font-bold relative left-10 text-white transition-all duration-75 hover:text-transparent [-webkit-text-stroke:2px_white]'
-          >Dream.</h1>
-          <h1 
-            className='w-full h-40 flex items-center text-[100px] font-bold relative left-10 -top-12 text-white transition-all duration-75 hover:text-transparent [-webkit-text-stroke:2px_white]'
-          >Repeat.</h1>     
-        </div>
-
-      </section>
     </>
   )
 }
 
-export default App
+export default LoginCard
