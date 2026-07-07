12892046ed237ae23e9f3797de2b30d6873c8ce9
K-Anant412 <anantkore412@gmail.com>
2026-07-07 18:47:44 +0530

    making more UI components

--- App_UI/src/pages/HomePage.jsx
+++ App_UI/src/pages/HomePage.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import SearchBar from '../utils/SearchBar'
 import { IoArrowUpCircleOutline } from "react-icons/io5";
 import { IoIosArrowForward } from "react-icons/io";
+import { RxDividerVertical } from "react-icons/rx";
 
 const HomePage = () => {
   return (
     <>
-        <section className='w-full h-full p-10 overflow-y-auto bg-olive-200 scrollbar-none '>
+        <section className='w-full h-full p-10 overflow-y-auto bg-[#FFF2DB] scrollbar-none '>
             {/* <SearchBar /> */}
-          <div id='header' className='relative w-full h-80 border-2 flex justify-start'>
+          <div id='header' className='relative w-full h-80 flex justify-start'>
 
             <div className='w-[35%] h-full flex flex-col justify-start relative shrink-0'>
                 <h1 className='text-7xl font-semibold'>Find Your</h1>
@@ -26,15 +27,15 @@ const HomePage = () => {
 
             <div className='w-full h-full flex items-center justify-evenly'>
 
-                <div className='w-[30%] h-[90%] border rounded-tl-[50%] rounded-tr-[50%] bg-linear-to-t  from-[#b02f61ee] via-[#831f44] to-[#5c112e] transition-all duration-200 shadow-2xl hover:scale-105'>
+                <div className='w-[30%] h-[90%] border border-[#cac8c1e4] rounded-tl-[50%] rounded-tr-[50%] bg-[#F8F5E9] transition-all duration-200 shadow-2xl hover:scale-105'>
 
                 </div>
 
-                <div className='w-[30%] h-[90%] border rounded-bl-[50%] rounded-br-[50%] bg-linear-to-t  from-[#b02f61ee] via-[#831f44] to-[#5c112e]'>
+                <div className='w-[30%] h-[90%] border border-[#cac8c1e4] rounded-bl-[50%] rounded-br-[50%] bg-[#F8F5E9] transition-all duration-200 shadow-2xl hover:scale-105'>
 
                 </div>
 
-                <div className='w-[30%] h-[90%] border rounded-tl-[50%] rounded-tr-[50%] bg-linear-to-t  from-[#b02f61ee] via-[#831f44] to-[#5c112e]'>
+                <div className='w-[30%] h-[90%] border border-[#cac8c1e4] rounded-tl-[50%] rounded-tr-[50%] bg-[#F8F5E9] transition-all duration-200 shadow-2xl hover:scale-105'>
                 
                 </div>
 
@@ -42,21 +43,71 @@ const HomePage = () => {
 
           </div>
 
-          <div id='bottom_header' className='w-full h-15 border-2 mt-2 flex items-center justify-around'>
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"></a>
+          <div id='bottom_header' className='w-full h-15 mt-2 flex items-center justify-around'>
+            
+            <div className="avatar flex items-center gap-1 rounded-2xl hover:bg-amber-50 h-full w-fit">
+              <div className="w-12 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+              <h1>New from Batman</h1>
+            <RxDividerVertical size={30} />
+            </div>
+
+
+            <div className="avatar flex items-center gap-1 rounded-2xl hover:bg-amber-50 h-full w-fit">
+              <div className="w-12 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+              <h1>New from Batman</h1>
+            <RxDividerVertical size={30} />
+            </div>
+
+
+            <div className="avatar flex items-center gap-1 rounded-2xl hover:bg-amber-50 h-full w-fit">
+              <div className="w-12 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+              <h1>New from Batman</h1>
+            <RxDividerVertical size={30} />
+            </div>
+
+
+            <div className="avatar flex items-center gap-1 rounded-2xl hover:bg-amber-50 h-full w-fit">
+              <div className="w-12 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+              <h1>New from Batman</h1>
+            <RxDividerVertical size={30} />
+            </div>
+
+            <div className="avatar flex items-center gap-1 rounded-2xl hover:bg-amber-50 h-full w-fit">
+              <div className="w-12 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+              <h1>New from Batman</h1>
+            <RxDividerVertical size={30} />
+            </div>
+
+            <div className="avatar flex items-center gap-1 rounded-2xl hover:bg-amber-50 h-full w-fit">
+              <div className="w-12 rounded">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+              <h1>New from Batman</h1>
+            <RxDividerVertical size={30} />
+            </div>
+            
           </div>
 
           <div className='relative w-full h-70 border-2 mt-2 flex flex-col p-2'>
-            <h1 className='text-2xl flex items-center justify-between w-full font-medium font-sans bg-amber-50 px-2'>
+            <h1 className='text-2xl flex items-center justify-between w-full font-medium px-2'>
               Recommended For You
               <a href="#" className='text-[20px] underline flex w-fit h-fit gap-1 items-center justify-center'>
                 see all
                 <IoIosArrowForward size={20} className='relative top-0.5 font-bold'/>
               </a>
             </h1>
+
+              
           </div>
 
           <div className='relative w-full h-70 border-2 mt-2 flex flex-col p-2'>
